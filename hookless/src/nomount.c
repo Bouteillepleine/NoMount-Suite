b3c15b12096157f992cc91ff9b762830400ed224
Bouteillepleine <steve0007.xda@gmail.com>
2026-08-09 23:30:19 +0200

    nomount: guard vma->vm_file restore in mmap on failure/reassign (upstream 8bdd8dd0)

--- hookless/src/nomount.c
+++ hookless/src/nomount.c
@@ -478,7 +478,7 @@ static int nm_mmap(struct file *file, struct vm_area_struct *vma)
 
     vma->vm_file = real_file;
     ret = real_file->f_op->mmap(real_file, vma);
-    vma->vm_file = file;
+    if (ret == 0 && vma->vm_file == real_file) vma->vm_file = file;
 
     return ret;
 }
@@ -496,7 +496,7 @@ static int nm_mmap_prepare(struct vm_area_desc *desc)
      * the pre-6.18 behaviour) and restore it afterwards. */
     *(struct file **)&desc->file = real_file;
     ret = real_file->f_op->mmap_prepare(desc);
-    *(struct file **)&desc->file = file;
+    if (ret == 0 && desc->file == real_file) *(struct file **)&desc->file = file;
 
     return ret;
 }
